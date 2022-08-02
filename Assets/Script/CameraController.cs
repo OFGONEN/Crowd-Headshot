5721a7976ca931e242a07a0b3460f3543940b8a7
ofgonen <ofgonen@gmail.com>
2022-08-02 19:09:36 +0300

    Implement CameraController
    - Update zoom level

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,38 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using FFStudio;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+
+public class CameraController : MonoBehaviour
+{
+#region Fields
+  [ Title( "Components" ) ]
+    [ SerializeField ] Camera _camera;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void OnCameraZoomLevelChange( float value )
+    {
+		_camera.fieldOfView = value;
+	}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
