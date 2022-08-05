613cc390b8bc3d773dc8aacc11279e6c5c3318f2
ofgonen <ofgonen@gmail.com>
2022-08-05 16:46:58 +0300

    Enemy: Starts idle

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -3,16 +3,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
 
 public class Enemy : MonoBehaviour
 {
 #region Fields
+  [ Title( "Setup" ) ]
+    [ SerializeField ] public bool enemy_is_walking;
 #endregion
 
 #region Properties
 #endregion
 
 #region Unity API
+    private void Awake()
+    {
+        if( !enemy_is_walking )
+			transform.forward = GameSettings.Instance.game_play_axis * -1f;
+	}
 #endregion
 
 #region API
