eb1ac94461f1a22679fd02baead5efd792f8dae6
ofgonen <ofgonen@gmail.com>
2022-08-12 12:29:03 +0300

    🐞 ExtensionMethod: ConvertV3_Z does not populate +Z parameter

--- Assets/Script/FFStudio/Utility/ExtensionMethods.cs
+++ Assets/Script/FFStudio/Utility/ExtensionMethods.cs
@@ -84,7 +84,7 @@ namespace FFStudio
 
 		public static Vector3 ConvertV3_Z( this Vector2 v2 )
 		{
-			return new Vector3( v2.x, v2.y, 0 );
+			return new Vector3( v2.x, 0, v2.y );
 		}
 
 		public static Vector3 RandomPointBetween( this Vector3 first, Vector3 second )
