7e53b2161f10e66f20f1a75ec6396f2760fdcf33
ofgonen <ofgonen@gmail.com>
2022-08-17 14:21:10 +0300

    🐞Pool_PowerLoot

--- Assets/Script/Pool_PowerLoot.cs
+++ Assets/Script/Pool_PowerLoot.cs
@@ -17,7 +17,7 @@ public class Pool_PowerLoot : ComponentPool< PowerLoot >
 		for( var i = 0; i < count; i++ )
 			GetEntity().Spawm( spawnPoint, divisor );
         
-        if( mod == 0 )
-			GetEntity().Spawm( spawnPoint, mod );
+		if( power < divisor || mod == 0 )
+			GetEntity().Spawm( spawnPoint, power );
 	}
 }
