e3f89726b7581694858e84dd3b1a5b500b791b38
ofgonen <ofgonen@gmail.com>
2022-08-17 16:13:19 +0300

    GameSetting: ScopeDuration
    - Remove power loot jump duration

--- Assets/Script/FFStudio/GameSettings.cs
+++ Assets/Script/FFStudio/GameSettings.cs
@@ -50,7 +50,6 @@ namespace FFStudio
     [ Title( "Loot" ) ]
         [ LabelText( "Loot Spawn Radius" ) ] public float loot_spawn_radius = 1f;
         [ LabelText( "Loot Spawn Jump Power" ) ] public float loot_spawn_jump_power = 1f;
-        [ LabelText( "Loot Spawn Jump Duration" ) ] public float loot_spawn_jump_duration = 0.35f; // ui scope on + off
         [ LabelText( "Loot Spawn Jump Ease" ) ] public Ease loot_spawn_jump_ease;
         [ LabelText( "Loot Spawn Travel Duration" ) ] public float loot_spawn_travel_duration = 0.35f;
         [ LabelText( "Loot Spawn Travel Ease" ) ] public Ease loot_spawn_travel_ease;
@@ -94,6 +93,8 @@ namespace FFStudio
 
         [ FoldoutGroup( "Debug" ) ] public float debug_ui_text_float_height;
         [ FoldoutGroup( "Debug" ) ] public float debug_ui_text_float_duration;
+
+        public float ScopeDuration => ui_crosshair_shoot_duration_on + ui_crosshair_shoot_duration_off;
 #endregion
 
 #region Fields (Singleton Related)
